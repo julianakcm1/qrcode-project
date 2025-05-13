dd5d36efecfb484482c021086bf39aa71fd267cf
Juliana <jkcm@ic.ufal.br>
2025-05-13 11:32:37 -0300

    feat: using chalk

--- src/prompts/prompt-main.js
+++ src/prompts/prompt-main.js
@@ -1,13 +1,15 @@
 // import { message } from "prompt";
 
+import chalk from "chalk";
+
 // SCHEMA
 const mainPrompt = [
   {
     name: "select",
-    description: "Choose the tool (1 - QR CODE) or (2 - PASSWORD)",
+    description: chalk.hex('#FFA500').bold("Choose the tool (1 - QR CODE) or (2 - PASSWORD)"),
     pattern: /^[1-2]+$/,
     // REGEX: ^inicio +$mais final
-    message: "Choose only between 1 and 2",
+    message: chalk.red.italic("Choose only between 1 and 2"),
     require: true
   }
 ]
