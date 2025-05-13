4e4a2b87886e8649bfcf800dabf25b8510a847b5
Juliana <jkcm@ic.ufal.br>
2025-05-13 11:23:08 -0300

    feat: choices on prompt

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import prompt from "prompt";
+import mainPrompt from "./prompts/prompt-main.js";
 
-async function main(params) {
-  
+async function main() {
+  prompt.get(mainPrompt, async (err, choise) => {
+    if(choise.select == 1) console.log("\n- Chose the QRCODE\n");
+    if(choise.select == 2) console.log("\n- Chose the PASSWORD\n");
+  })
+  prompt.start();
 }
 
 main();
--- src/prompts/prompt-main.js
+++ src/prompts/prompt-main.js
@@ -1,12 +1,15 @@
-import { message } from "prompt";
+// import { message } from "prompt";
 
+// SCHEMA
 const mainPrompt = [
   {
     name: "select",
-    description: "Choose the tool (1 - QR CODE or 2 - PASSWORD)",
+    description: "Choose the tool (1 - QR CODE) or (2 - PASSWORD)",
     pattern: /^[1-2]+$/,
     // REGEX: ^inicio +$mais final
     message: "Choose only between 1 and 2",
     require: true
   }
 ]
+
+export default mainPrompt;
